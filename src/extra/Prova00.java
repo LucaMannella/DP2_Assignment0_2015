bd2b7a8b524f5ae1e680b5f4c7d5f5cb6bb94fe3
Luke <luca.mannella@outlook.it>
2015-11-26 15:29:00 +0100

    another test

--- src/extra/Prova00.java
+++ src/extra/Prova00.java
@@ -11,10 +11,16 @@ public class Prova00 {
 		String aName = "MyAction1";
 		String role = "MyRole1";
 		String id = wfrName+"_"+aName;
-		
-		String fields = "id=\""+id+"\" name=\""+aName+"\" role=\""+role+"\"";
-		System.out.println("The builded String is: \n\t"+fields);
 
+		String fields = "id=\""+id+"\" name=\""+aName+"\" role=\""+role+"\"";
+		System.out.println("The builded String is: \n\t"+fields+"\n");
+		
+		System.out.println("<WorkflowManager>");
+		System.out.println("\t<workflow name=\""+wfrName+"\">");
+		System.out.println("\t\t<action "+fields+">");
+		System.out.println("\t\t</action>");
+		System.out.println("\t</workflow>");
+		System.out.println("</WorkflowManager>");
 	}
 
 }
